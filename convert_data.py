e8872bf26a6fd5ea116e04a3c657faf86444ffbb
Eugene <genesith@kaist.ac.kr>
2020-11-02 16:55:51 +0900

    process data

--- convert_data.py
+++ convert_data.py
@@ -2,23 +2,53 @@ import os
 import json
 import nltk
 
+def replace_special_tokens(str_):
+    special_tokens = [".", ",", "?", "/", ";", ":", "<", ">", "[", "]", "/", "|", "'", '"', "{", "}",
+                      "-", "+", ")", "(", "_", "=", "!", "*", "&", "^", "%", "$", "#", "@", "~", "`",
+                      "’", "“", "”", "–", "—"]
+    for token_ in special_tokens:
+        str_ = str_.replace(token_, " ")
+    return str_.strip().replace("  ", " ")
+
 class AnnSent():
     def __init__(self, sent):
         self.sent = sent
-        self.atts = []
+        self.pre_atts = []
 
     def annotate_part(self, text, att_type, source):
-        self.atts.append((text,att_type,source))
+        self.pre_atts.append((text,att_type,source))
+
+
+    def process_data(self):
+        self.sent = replace_special_tokens(self.sent)
+        self.attitudes = []
+        for att in self.pre_atts:
+            text, att_type, source = att
+            text = replace_special_tokens(text)
+            attitude_info = {"att_type": att_type, "source": source}
+            tokens = nltk.word_tokenize(self.sent)
+            tokens = nltk.word_tokenize(text)
+            if self.sent.count(text) == 1:
+                trigger = {"text": text}
+            elif self.sent.count(text) == 0:
+                assert False
+            else:
+                print("duplicates")
+                print(text)
+                print(self.sent)
+                print("_____")
+
 
 def find_the_sent(indexes_list, start_index):
     # finds the sentence an annotated part belongs to, using the start index
     for i in range(len(indexes_list)):
-        if(start_index > indexes_list[i]):
-            continue
-        else:
+        if(start_index < indexes_list[i]):
             break
-
-    return i
+        else:
+            continue
+    if start_index>=indexes_list[-1]:
+        return len(indexes_list)-1
+    return i -1
 
 
 def convert_Data(dir, datapath, targetpath):
@@ -48,7 +78,7 @@ def convert_Data(dir, datapath, targetpath):
     sent_starts.sort()
 
     for sent_start in sent_starts:
-        start,end = sents[start]
+        start,end = sents[sent_start]
         sentence = doc[start:end]
         annsents.append(AnnSent(sentence))
 
@@ -99,10 +129,18 @@ def convert_Data(dir, datapath, targetpath):
         att_text = doc[start:end]
 
         sent_num = find_the_sent(sent_starts, start)
-        annsents[sent_num].annotate_part(att_text, att_type, source)
+        if att_text in annsents[sent_num].sent:
+            annsents[sent_num].annotate_part(att_text, att_type, source)
+        elif att_text in annsents[sent_num].sent+'"': #one error due to quotation mark being outside
+            annsents[sent_num].sent= annsents[sent_num].sent + '"'
+            annsents[sent_num].annotate_part(att_text, att_type, source)
+        else:
+            print("skipping due to sentence split error") #4 errors because missplit sentence
 
 
 
+    for annsent in annsents:
+        annsent.process_data()
 
 
 
